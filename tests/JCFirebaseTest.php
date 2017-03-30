4fce890d980279af5e590737c7253a8d5c26d6be
Jared Chu <me@jaredchu.com>
2017-03-30 21:18:10 +0700

    reformat code

--- tests/JCFirebaseTest.php
+++ tests/JCFirebaseTest.php
@@ -10,128 +10,139 @@
 use JCFirebase\JCFirebase;
 use JCFirebase\Option;
 
-class JCFirebaseTest extends PHPUnit_Framework_TestCase {
-	const FIREBASE_URI = 'https://fir-php-test-c7fa2.firebaseio.com/';
-	const KEY_FILE = '/resource/firebase-php-test-0a49b34e5f4a.json';
+class JCFirebaseTest extends PHPUnit_Framework_TestCase
+{
+    const FIREBASE_URI = 'https://fir-php-test-c7fa2.firebaseio.com/';
+    const KEY_FILE = '/resource/firebase-php-test-0a49b34e5f4a.json';
 
-	/**
-	 * @var JCFirebase
-	 */
-	protected static $firebase;
+    /**
+     * @var JCFirebase
+     */
+    protected static $firebase;
 
-	public static function setUpBeforeClass() {
-		self::$firebase = JCFirebase::fromKeyFile( self::FIREBASE_URI, getcwd() . self::KEY_FILE );
-	}
+    public static function setUpBeforeClass()
+    {
+        self::$firebase = JCFirebase::fromKeyFile(self::FIREBASE_URI, getcwd() . self::KEY_FILE);
+    }
 
-	public function testGetPathURI() {
-		$firebase = self::$firebase;
+    public function testGetPathURI()
+    {
+        $firebase = self::$firebase;
 
-		self::assertEquals( self::FIREBASE_URI . '.json', $firebase->getPathURI() );
-		self::assertEquals( self::FIREBASE_URI . '.json', $firebase->getPathURI( '/' ) );
+        self::assertEquals(self::FIREBASE_URI . '.json', $firebase->getPathURI());
+        self::assertEquals(self::FIREBASE_URI . '.json', $firebase->getPathURI('/'));
 
-		self::assertEquals( self::FIREBASE_URI . 'path.json', $firebase->getPathURI( 'path' ) );
-		self::assertEquals( self::FIREBASE_URI . 'path.json', $firebase->getPathURI( '/path/' ) );
-		self::assertEquals( self::FIREBASE_URI . 'path.json', $firebase->getPathURI( '//path//' ) );
+        self::assertEquals(self::FIREBASE_URI . 'path.json', $firebase->getPathURI('path'));
+        self::assertEquals(self::FIREBASE_URI . 'path.json', $firebase->getPathURI('/path/'));
+        self::assertEquals(self::FIREBASE_URI . 'path.json', $firebase->getPathURI('//path//'));
 
-		self::assertEquals( self::FIREBASE_URI . 'path/to/your.json', $firebase->getPathURI( 'path/to/your' ) );
-		self::assertEquals( self::FIREBASE_URI . 'path/to/your.json', $firebase->getPathURI( '/path/to/your/' ) );
-		self::assertEquals( self::FIREBASE_URI . 'path/to/your.json', $firebase->getPathURI( '//path/to/your//' ) );
+        self::assertEquals(self::FIREBASE_URI . 'path/to/your.json', $firebase->getPathURI('path/to/your'));
+        self::assertEquals(self::FIREBASE_URI . 'path/to/your.json', $firebase->getPathURI('/path/to/your/'));
+        self::assertEquals(self::FIREBASE_URI . 'path/to/your.json', $firebase->getPathURI('//path/to/your//'));
 
-	}
+    }
 
-	public function testGet() {
-		$firebase = self::$firebase;
+    public function testGet()
+    {
+        $firebase = self::$firebase;
 
-		$response = $firebase->get();
-		self::assertEquals( 200, $response->status_code );
-	}
+        $response = $firebase->get();
+        self::assertEquals(200, $response->status_code);
+    }
 
-	public function testPut() {
-		$firebase = self::$firebase;
-		$subPath  = 'put_test';
+    public function testPut()
+    {
+        $firebase = self::$firebase;
+        $subPath = 'put_test';
 
-		$response = $firebase->put( $subPath, array(
-			'data' => self::data()
-		) );
+        $response = $firebase->put($subPath, array(
+            'data' => self::data()
+        ));
 
-		self::assertEquals( 200, $response->status_code );
-		self::assertEquals( 1, json_decode( $response->body )->number );
-		self::assertEquals( 'hello', json_decode( $response->body )->string );
-	}
+        self::assertEquals(200, $response->status_code);
+        self::assertEquals(1, json_decode($response->body)->number);
+        self::assertEquals('hello', json_decode($response->body)->string);
+    }
 
-	private function data() {
-		return array(
-			'number' => 1,
-			'string' => 'hello'
-		);
-	}
+    private function data()
+    {
+        return array(
+            'number' => 1,
+            'string' => 'hello'
+        );
+    }
 
-	public function testPost() {
-		$firebase = self::$firebase;
-		$subPath  = 'post_test';
+    public function testPost()
+    {
+        $firebase = self::$firebase;
+        $subPath = 'post_test';
 
-		$response = $firebase->post( $subPath, array(
-			'data' => self::data()
-		) );
+        $response = $firebase->post($subPath, array(
+            'data' => self::data()
+        ));
 
-		self::assertEquals( 200, $response->status_code );
-		self::assertNotNull( json_decode( $response->body )->name );
-	}
+        self::assertEquals(200, $response->status_code);
+        self::assertNotNull(json_decode($response->body)->name);
+    }
 
-	public function testPatch() {
-		$firebase = self::$firebase;
-		$subPath  = 'patch_test';
+    public function testPatch()
+    {
+        $firebase = self::$firebase;
+        $subPath = 'patch_test';
 
-		$firebase->put( $subPath, array(
-			'data' => self::data()
-		) );
+        $firebase->put($subPath, array(
+            'data' => self::data()
+        ));
 
-		$response = $firebase->patch( $subPath, array(
-			'data' => array(
-				'number' => 2,
-				'string' => 'hello2'
-			)
-		) );
+        $response = $firebase->patch($subPath, array(
+            'data' => array(
+                'number' => 2,
+                'string' => 'hello2'
+            )
+        ));
 
-		self::assertEquals( 200, $response->status_code );
-		self::assertEquals( 2, json_decode( $response->body )->number );
-		self::assertEquals( 'hello2', json_decode( $response->body )->string );
-	}
+        self::assertEquals(200, $response->status_code);
+        self::assertEquals(2, json_decode($response->body)->number);
+        self::assertEquals('hello2', json_decode($response->body)->string);
+    }
 
-	public function testDelete() {
-		$firebase = self::$firebase;
-		$subPath  = 'delete_test';
+    public function testDelete()
+    {
+        $firebase = self::$firebase;
+        $subPath = 'delete_test';
 
-		$firebase->put( $subPath, array(
-			'data' => self::data()
-		) );
+        $firebase->put($subPath, array(
+            'data' => self::data()
+        ));
 
-		$subPath = 'delete_test/number';
+        $subPath = 'delete_test/number';
 
-		$response = $firebase->delete( $subPath );
+        $response = $firebase->delete($subPath);
 
-		self::assertEquals( 200, $response->status_code );
-	}
+        self::assertEquals(200, $response->status_code);
+    }
 
-	public function testGetShallow() {
-		$firebase = self::$firebase;
-		$subPath  = 'get_shallow_test';
+    public function testGetShallow()
+    {
+        $firebase = self::$firebase;
+        $subPath = 'get_shallow_test';
 
-		$firebase->put( $subPath, array( 'data' => self::data() ) );
+        $firebase->put($subPath, array('data' => self::data()));
 
-		$response = $firebase->getShallow( $subPath );
-		self::assertEquals( 200, $response->status_code );
-		self::assertTrue( json_decode( $response->body )->number );
-		self::assertTrue( json_decode( $response->body )->string );
-	}
+        $response = $firebase->getShallow($subPath);
+        self::assertEquals(200, $response->status_code);
+        self::assertTrue(json_decode($response->body)->number);
+        self::assertTrue(json_decode($response->body)->string);
+    }
 
-	public function testGetPrint() {
-		$firebase = self::$firebase;
+    public function testGetPrint()
+    {
+        $firebase = self::$firebase;
 
-		self::assertContains( " ", $firebase->get( null, array(
-			Option::OPTION_PRINT => Option::PRINT_PRETTY
-		) )->body );
+        self::assertContains(" ", $firebase->get(null, array(
+            Option::OPTION_PRINT => Option::PRINT_PRETTY
+        ))->body);
 
-		self::assertTrue($firebase->isValid());
-	}
+        self::assertTrue($firebase->isValid());
+    }
 }
